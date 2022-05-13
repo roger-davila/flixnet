efcb525136e9fd9851f9de66e5956bdf1003265b
Roger Davila <rogerddavila@gmail.com>
2022-05-13 14:17:08 -0700

    add user login and logout

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      # redirect user to orders after they long it
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message }
+  return render(request, 'registration/signup.html', context)
