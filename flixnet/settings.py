802d35b6d87bfe93d452fd289afdfa448416380b
Roger Davila <rogerddavila@gmail.com>
2022-05-18 13:11:07 -0700

    config deployment

--- flixnet/settings.py
+++ flixnet/settings.py
@@ -81,10 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'flixnet',
-        'USER': 'prkrsamuels',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': ''
     }
 }
 
@@ -134,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
