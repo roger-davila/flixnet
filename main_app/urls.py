c4469f3f5a34ceb8c60e6c3b37e325eb07b119b7
prkrsamuels7 <prkrsamuels@protonmail.com>
2022-05-19 19:39:56 -0400

    signin/signup/login style

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from main_app.forms import UserLoginForm
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('accounts/signup/', views.signup, name='signup'),
+  path('accounts/login/', LoginView.as_view(authentication_form = UserLoginForm), name="login"),
   path('movies/search/', views.search, name='search'),
   path('movies/<int:movie_id>', views.movie_detail, name='movie_detail'),
   path('shipping_address/create/', views.ShippingAddressCreate.as_view(), name='shipping_address_create'),
